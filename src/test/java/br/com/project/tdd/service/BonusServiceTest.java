eed7407719336b07e665798ffd32ffaf57e12634
erikbernard <erikbrsilva@gmail.com>
2023-07-23 18:36:26 -0300

    refactoring tests

--- src/test/java/br/com/project/tdd/service/BonusServiceTest.java
+++ src/test/java/br/com/project/tdd/service/BonusServiceTest.java
@@ -6,31 +6,34 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import br.com.project.tdd.modelo.Funcionario;
 
 class BonusServiceTest {
 
+	private BonusService bonusService;
+
+	@BeforeEach
+	public void inicializar() {
+		this.bonusService = new BonusService();
+	}
+
 	@Test
 	void bonusDeveriaSerZeroParaFuncionarioComSalarioMuitoAlto() {
-		var bonusService = new BonusService();
 		assertThrows(IllegalArgumentException.class, () -> bonusService.calcularBonus(new Funcionario("Rodrigo", LocalDate.now(), new BigDecimal("25000"))));
 	}
 
 	@Test
 	void bonusDeveriaSer10PorCentoDoSalario() {
-		var bonusService = new BonusService();
 		var bonus = bonusService.calcularBonus(new Funcionario("Rodrigo", LocalDate.now(), new BigDecimal("2500")));
-
 		assertEquals(new BigDecimal("250.00"), bonus);
 	}
 
 	@Test
 	void bonusDeveriaSer10PorCentoParaSalarioDeExatamente10000() {
-		var bonusService = new BonusService();
 		var bonus = bonusService.calcularBonus(new Funcionario("Rodrigo", LocalDate.now(), new BigDecimal("10000")));
-
 		assertEquals(new BigDecimal("1000.00"), bonus);
 	}
 
--- src/test/java/br/com/project/tdd/service/ReajusteServiceTest.java
+++ src/test/java/br/com/project/tdd/service/ReajusteServiceTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import br.com.project.tdd.modelo.Desempenho;
@@ -12,31 +13,31 @@ import br.com.project.tdd.modelo.Funcionario;
 
 public class ReajusteServiceTest {
 
+    private ReajusteService reajusteService;
+    private Funcionario funcionario;
+
+    @BeforeEach
+    public void Inicializar() {
+        this.reajusteService = new ReajusteService();
+        this.funcionario = new Funcionario("Rodrigo", LocalDate.now(), new BigDecimal("1000.00"));
+    }
+
     @Test
     public void reajusteDeveriaSerDeTresPorCentoQuandoDesempenhoForADesejar() {
-        var reajusteService = new ReajusteService();
-        var Funcionario = new Funcionario("Rodrigo", LocalDate.now(), new BigDecimal("1000.00"));
-        reajusteService.concederReajuste(Funcionario, Desempenho.A_DESEJAR);
-
-        assertEquals(new BigDecimal("1030.00"), Funcionario.getSalario());
+        reajusteService.concederReajuste(funcionario, Desempenho.A_DESEJAR);
+        assertEquals(new BigDecimal("1030.00"), funcionario.getSalario());
     }
 
     @Test
     public void reajusteDeveriaSerDeQuinzePorCentoQuandoDesempenhoForBom() {
-        var reajusteService = new ReajusteService();
-        var Funcionario = new Funcionario("Rodrigo", LocalDate.now(), new BigDecimal("1000.00"));
-        reajusteService.concederReajuste(Funcionario, Desempenho.BOM);
-
-        assertEquals(new BigDecimal("1150.00"), Funcionario.getSalario());
+        reajusteService.concederReajuste(funcionario, Desempenho.BOM);
+        assertEquals(new BigDecimal("1150.00"), funcionario.getSalario());
     }
 
     @Test
     public void reajusteDeveriaSerDeVintePorCentoQuandoDesempenhoForOtimo() {
-        var reajusteService = new ReajusteService();
-        var Funcionario = new Funcionario("Rodrigo", LocalDate.now(), new BigDecimal("1000.00"));
-        reajusteService.concederReajuste(Funcionario, Desempenho.OTIMO);
-
-        assertEquals(new BigDecimal("1200.00"), Funcionario.getSalario());
+        reajusteService.concederReajuste(funcionario, Desempenho.OTIMO);
+        assertEquals(new BigDecimal("1200.00"), funcionario.getSalario());
     }
     
 }
